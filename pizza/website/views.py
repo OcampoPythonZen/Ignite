87690c6d3f3905678cc695f2d4afe5acafdf7a58
OcampoPythonZen <eocampo1986@gmail.com>
2019-03-03 22:57:49 -0600

    Save method was used to keep save the Topping Pizza. Time's missing. :(

--- pizza/website/views.py
+++ pizza/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ToppingForm, PizzaForm, OrderForm
+from .models import Topping, Pizza, Order
 
 def home(request):
 	template_name = 'website/home_view.html'
@@ -12,11 +13,43 @@ def order(request):
 	return render(request, template_name, {'form':form})
 
 def pizza(request):
+	# if request.method == 'POST':
+	# 	form = PizzaForm(request.POST)
+	# 	if form.is_valid():
+	# 		data = form.cleaned_data
+	# 		pizza_name = data.get('pizza_name')
+	# 		size = data.get('size')
+	# 		toppings = data.get('toppings')
+	# 		price_pizza = data.get('price_pizza')
+	# 		db_register = Pizza(
+	# 			pizza_name = pizza_name,
+	# 			size = size,
+	# 			toppings = toppings,
+	# 			price_pizza = price_pizza
+	# 		)
+	# 		db_register.save()
+	# else:
 	form = PizzaForm()
 	template_name = 'website/pizza_form.html'
 	return render(request, template_name, {'form':form})
 
 def topping(request):
-	form = ToppingForm()
+	if request.method == 'POST':
+		form = ToppingForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			topping_name = data.get('topping_name')
+			calories = data.get('calories')
+			unit = data.get('unit')
+			price_topping = data.get('price_topping')
+			db_register = Topping(
+				topping_name = topping_name,
+				calories = calories,
+				unit = unit,
+				price_topping = price_topping
+			)
+			db_register.save()
+	else:
+		form = ToppingForm()
 	template_name = 'website/topping_form.html'
 	return render(request, template_name, {'form':form})
