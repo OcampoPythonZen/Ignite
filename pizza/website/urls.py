023af732b431a10cc0f93a81fb1656ccb8f2d03c
OcampoPythonZen <eocampo1986@gmail.com>
2019-03-03 20:01:39 -0600

    Url added to create the toppings, pizza and order

--- pizza/website/urls.py
+++ pizza/website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from website.views import home, order
+from website.views import home, order, pizza, topping
 
 urlpatterns = [
     path('',home),
     path('new/',order),
+    path('pizza/',pizza),
+    path('topping/',topping),
 ]
--- pizza/website/views.py
+++ pizza/website/views.py
@@ -10,3 +10,13 @@ def order(request):
 	form = OrderForm()
 	template_name = 'website/order_form.html'
 	return render(request, template_name, {'form':form})
+
+def pizza(request):
+	form = PizzaForm()
+	template_name = 'website/pizza_form.html'
+	return render(request, template_name, {'form':form})
+
+def topping(request):
+	form = ToppingForm()
+	template_name = 'website/topping_form.html'
+	return render(request, template_name, {'form':form})
